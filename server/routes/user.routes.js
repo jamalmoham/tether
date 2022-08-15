c78b56069a60f7374c7f3b2637844bca5fd870a2
Jamal Muniru <jamalmoham10@gmail.com>
2022-08-14 21:14:46 -0400

    login/logout

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -8,9 +8,7 @@ const Users = require("../controllers/user.controller");
 const { authenticate } = require("../config/jwt.config");
 module.exports = (app) => {
   app.post("/api/register", Users.createUser);
-  // app.post("/api/login", Users.login);
-//   app.get("/api/users", authenticate, Users.getAll);
+  app.post("/api/login", Users.login);
+  app.get("/api/logout", Users.logout);
 };
 
-
-//hey hey dghsrtjryyy
